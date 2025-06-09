45d444925053d69b8d52f28387b8b548b7f0b272
Jennifer Campbell <jennifercampbell28@hotmail.com>
2025-06-09 11:21:05 +0100

    added endpoints info file

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const getRecipe = require("./gemini/controllers/recipe.controller");
+const endpoints = require("./endpoints.json")
 
 app.use(express.json());
 app.use(cors());
+
+app.get("/api", (req, res) => {
+    res.status(200).send({endpoints})
+})
 
 app.post("/api/generate-recipe", getRecipe)
 
